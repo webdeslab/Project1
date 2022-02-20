b01fccd9076d71535ffef2dd9ea54aca61ac71a5
Vitalii <mr.nutsa@gmail.com>
2022-02-20 20:38:54 +0200

    added new object in script.js

--- script.js
+++ script.js
@@ -23,3 +23,13 @@
 
 'use strict';
 
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
