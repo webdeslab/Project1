9edc882235d137f4dd7d7d445263d151d5da0351
Vitalii <mr.nutsa@gmail.com>
2022-02-20 16:36:00 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+"use strict";
+// Объект
+// const obj ={
+//     name: 'Vitaly',
+//     surname: 'Nutsa',
+//     age: 27
+// };
+
+
+// Массив
+// console.log(obj);
+
+// let arr = ['pink', 'red', 'blue', 'white', 'black', 'yellow', 'green'];
+
+// console.log(arr[1]);
+
+// const answers = [];
+
+// answers[0] = prompt('Ваше имя?','');
+// answers[1] = prompt('Ваша фамилия?','');
+// answers[2] = prompt('Ваш возраст?','');
+
+// document.write(answers);
+
+
+// Интерполяция
+// const user = 'Vitaly';
+// const surname = 'Nutsa';
+
+// console.log(`Hello ${user} ${surname}`);
+
+// let firstNumber = 10,
+//     secondNumber = 10;
+
+// console.log(firstNumber || secondNumber + 5 == 15);
