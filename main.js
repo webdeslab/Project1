09ed2114a3ed25779e3f4107882f724283de013b
Vitalii Nutsa <mr.nutsa@gmail.com>
2022-02-21 23:11:28 +0200

    added new term in files main.js, and script.js

--- main.js
+++ main.js
@@ -33,3 +33,27 @@
 //     secondNumber = 10;
 
 // console.log(firstNumber || secondNumber + 5 == 15);
+
+// let areYouReady = prompt ('Ты готов идти до конца и получить свой первый оффер?', '');
+
+// if (areYouReady == 'yes') {
+//         alert('Продолжай в том же духе!');
+// } else if (areYouReady == null, false) {
+//     areYouReady = prompt ('Только ответ Yes or No!');
+// } else {
+//     alert('Тогда продолжай тратить время на занятие которое тебе не по душе...');
+// }
+
+
+// if (a + b == c) {
+//     console.log('Попробуй еще разок!');
+// } else if (a + b < c) {
+//     console.log('Красавчик!');
+// } else {
+//     console.log('Ничего не вышло:(');
+// }
+
+const a = 45,
+      b = 50,
+      c = 100,
+      d = 200;
