b19ecd10e058c657d91635b6971f142650a4f34f
Vitalii <mr.nutsa@gmail.com>
2022-02-21 22:02:49 +0200

    added new object

--- script.js
+++ script.js
@@ -33,3 +33,12 @@ const personalMovieDB = {
     privat: false
 };
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
