132c5b34c6d1edd1c1a25a9c95064f3796716dd3
Vitalii Nutsa <mr.nutsa@gmail.com>
2022-06-13 11:35:59 +0300

    commit

--- script.js
+++ script.js
@@ -89,22 +89,22 @@
 // //     num--;
 // // }
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-let a = prompt("Один из последних просмотренных фильмов?",""),
-    b = prompt("На сколько оцените его?",""),
-    c = prompt("Один из последних просмотренных фильмов?",""),
-    d = prompt("На сколько оцените его?","");
+// let a = prompt("Один из последних просмотренных фильмов?",""),
+//     b = prompt("На сколько оцените его?",""),
+//     c = prompt("Один из последних просмотренных фильмов?",""),
+//     d = prompt("На сколько оцените его?","");
 
-personalMovieDB.movies[a] = [b];
-personalMovieDB.movies[c] = [d];
+// personalMovieDB.movies[a] = [b];
+// personalMovieDB.movies[c] = [d];
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
