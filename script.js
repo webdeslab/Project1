f3c9572f1eb07c1e14192ab051b43fe2227aaa5f
Vitalii Nutsa <mr.nutsa@gmail.com>
2022-06-13 18:15:37 +0300

    functions added

--- script.js
+++ script.js
@@ -108,3 +108,187 @@
 // personalMovieDB.movies[c] = [d];
 
 // console.log(personalMovieDB);
+
+// console.log( NaN || 2 || undefined );
+
+// console.log( NaN && 2 && undefined );
+
+// console.log( 1 && 2 && 3 );
+
+// console.log( !1 && 2 || !3 );
+
+// console.log( 25 || null && !3 );
+
+// console.log( NaN || null || !3 || undefined || 5);
+
+// console.log( NaN || null && !3 && undefined || 5);
+
+// console.log( 5 === 5 && 3 > 1 || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//     console.log('Done!');
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//     console.log('Done!');
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//     console.log('Done!');
+// } else {
+//     console.log('До свидания!');
+// }
+
+// function firstTask() {
+
+//     for (let i = 4; i <= 10; i++) {
+//         console.log(i);
+//     }
+// }
+
+// firstTask();
+
+// function secondTask() {
+//     // Пишем решение вот тут
+//     for (let j = 20; j >= 10; j--) {
+//         if (j === 13) {
+//             break;
+//         }
+//         console.log(j);
+//     }
+// }
+
+// secondTask();
+
+// function thirdTask() {
+//     // Пишем решение вот тут
+//     for (let k = 2; k <= 10; k++) {
+//         if (k % 2 === 0) {
+//             console.log(k);
+//         }
+//     }
+// }
+
+// thirdTask();
+
+// for (let i = 2; i <= 16; i++) {
+//          if (i % 2 === 0) {
+//              continue;
+//         } else {
+//              console.log(i);
+//     }
+// }
+
+// function fourthTusk() {
+//     let i = 2;
+//     while (i <= 16) {
+//         if(i % 2 === 0) {
+//             i++;
+//             continue;
+//         } else {
+//             console.log(i);
+//         }
+//         i++;
+//     }
+// }
+
+// fourthTusk();
+
+// function fifthTask() {
+//     const arrayOfNumbers = [];
+
+//     // Пишем решение вот тут
+//     for (let i = 5; i <= 10; i++) {
+//         arrayOfNumbers[i - 5] = i;
+//     }
+    
+//     // Не трогаем
+//     console.log(arrayOfNumbers);
+//     return arrayOfNumbers;
+// }
+
+// fifthTask();
+
+// function firstTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const arr = [3, 5, 8, 16, 20, 23, 50];
+//     const result = [];
+
+//     // Пишем решение вот тут
+//     for(let i = 0; i < arr.length; i++) {
+//         result[i] = arr[i];
+//     }
+    
+//     // Не трогаем
+//     console.log(result);
+//     return result;
+// }
+
+// firstTask();
+
+// function secondTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+//     // Пишем решение вот тут
+//     for(let i = 0; i < data.length; i++) {
+//         if (typeof(data[i]) === 'number') {
+//             data[i] = data[i] * 2;
+//         } else if (typeof(data[i]) === 'string') {
+//             data[i] = `${data[i]} - Done`;
+//         }
+//     }
+    
+//     // Не трогаем
+//     console.log(data);
+//     return data;
+// }
+
+// secondTask();
+
+// function thirdTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+//     const result = [];
+
+//     // Пишем решение вот тут
+//     for (let i = 1; i <= data.length; i++) {
+//         result[i - 1] = data[data.length - i];
+//     }
+    
+//     // Не трогаем
+//     console.log(result);
+//     return result;
+// }
+
+// thirdTask();
+
+// const lines = 5;
+// let result = '';
+// // Проверяется именно переменная result, формируйте строку в ней
+// for (let i = 0; i <= lines; i++) {
+//     for(let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for(let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+// console.log(result);
+
