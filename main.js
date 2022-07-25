749164d3b09fb2da875226b26f1583ea0006477c
Vitalii Nutsa <mr.nutsa@gmail.com>
2022-07-25 22:41:15 +0300

    working with functions

--- main.js
+++ main.js
@@ -57,3 +57,78 @@
 //       b = 50,
 //       c = 100,
 //       d = 200;
+
+const numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('good');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+
+function showMyDb () {
+    
+}
+
+console.log(personalMovieDB);
+
+for (let i = 1; 1 < 6; i++) {
+    if (i == 6) {
+        break;
+    }
+    console.log(i);
+}
+
+let num = 8;
+while (num) {
+    console.log(num);
+    num--;
+}
+
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// let a = prompt("Один из последних просмотренных фильмов?",""),
+//     b = prompt("На сколько оцените его?",""),
+//     c = prompt("Один из последних просмотренных фильмов?",""),
+//     d = prompt("На сколько оцените его?","");
+
+// personalMovieDB.movies[a] = [b];
+// personalMovieDB.movies[c] = [d];
+
+// console.log(personalMovieDB);
