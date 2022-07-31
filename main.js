432d3865bda69a7f79e044c0f89ff88553bcfde9
Vitalii Nutsa <mr.nutsa@gmail.com>
2022-08-01 00:57:41 +0300

    some code added

--- main.js
+++ main.js
@@ -288,4 +288,24 @@ const styles = {
     }
 };
 
-console.log(Object.keys().length);
+console.log(Object.keys(styles).length);
+
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    }
+};
+
+function showExperience(plan) {
+    const {exp} = plan.skills;
+    return exp;
+}
+
+console.log(showExperience(personalPlanPeter));
