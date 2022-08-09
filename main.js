4cf793c08de6e304930dfc73ee6abf314804d60f
Vitalii Nutsa <mr.nutsa@gmail.com>
2022-08-09 22:56:36 +0300

    fixed project

--- main.js
+++ main.js
@@ -464,3 +464,141 @@ const personalMovieDB = {
         });
     }
 };
+
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+function isBudgetEnough(data) {
+    let square = 0;
+    let volume = 0;
+
+    data.shops.forEach(shop => {
+        square += shop.width * shop.length;
+    });
+
+    volume = data.height * square;
+
+    if (data.budget - (volume * data.moneyPer1m3) >= 0) {
+        return 'Бюджета достаточно';
+    }else {
+        return 'Бюджета недостаточно';
+    }
+}
+
+console.log(isBudgetEnough(shoppingMallData));
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    const a = [], b = [], c = [], rest = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);
+        }else if (i < 6) {
+            b.push(arr[i]);
+        }else if (i < 9) {
+            c.push(arr[i]);
+        }else {
+            rest.push(arr[i]);
+        }
+    }
+
+    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-': rest.join(', ')}`];
+ }
+
+ console.log(sortStudentsByGroups(students));
+
+function createCounter () {
+    let counter = 0;
+    
+    const myFunction = function () {
+        counter = counter + 1;
+        return counter;
+    };
+
+    return myFunction;
+}
+
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+
+console.log(c1,c2,c3);
+
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
+
+function isOpen(prop) {
+    let answer = '';
+    prop ? answer = 'Открыто' : answer = 'Закрыто';
+
+    return answer;
+}
+
+console.log(isOpen(restorantData.openNow));
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+    if (+fDish.price.slice(0, -1) + (sDish.price.slice(0,-1)) < +average.slice(0,-1)) {
+        return 'Цена ниже средней';
+    } else {
+        return 'Цена выше средней';
+    }
+}
+
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
+
+    copy.waitors = [{name: 'Mike', age: 32}];
+    return copy;
+}
+
+transferWaitors(restorantData);
